3280c4be8d18f565c684d342dadb8d56cc7f97ec
jeanpierre <jean.barbary@wanadoo.fr>
2021-03-05 18:18:02 +0100

    wrong file !

--- npds_quiz/install.conf.php
+++ npds_quiz/install.conf.php
@@ -1,99 +1,105 @@
 <?php
 /************************************************************************/
-/* DUNE by NPDS                                                         */
-/*                                                                      */
-/* NPDS Copyright (c) 2002-2020 by Philippe Brunier                     */
-/*                                                                      */
-/* This program is free software. You can redistribute it and/or modify */
-/* it under the terms of the GNU General Public License as published by */
-/* the Free Software Foundation; either version 2 of the License.       */
-/*                                                                      */
-/* NMIG : NPDS Module Installer Generator                               */
-/* ---------------------------------------------------------------------*/
-/* Version 2.0 - 2015                                                   */
-/* Générateur de fichier de configuration pour Module-Install 1.1       */
-/* Développé par Boris - http://www.lordi-depanneur.com                 */
-/* Module-Install est un installeur inspiré du programme d'installation */
-/* d'origine du module Hot-Projet développé par Hotfirenet              */
-/* ---------------------------------------------------------------------*/
-/* Version 2.1 for NPDS 16 jpb 2017                                     */
+/* NPDS : Net Portal Dynamic System                                     */
+/* ================================                                     */
+/* This version name NPDS Copyright (c) 2001-2020 by Philippe Brunier   */
 /************************************************************************/
-
+/* Original Copyright (c) [ 2019 ] Par [ NICOL ]                        */
+/* Module   : [ QUIZ ]                                                  */
+/* Auteur   : [ NOM DU DEVELOPPEUR ]                                    */
+/* Mail     : [ MAIL DU DEVELOPPEUR ]                                   */
+/* Site     : [ SITE DU DEVELOPPEUR ]                                   */
+/************************************************************************/
+/* MODULE DEVELOPPE POUR NPDS VERSION [ REvolution v.16.0 ]             */
+/************************************************************************/
+/*                                                                      */
+/************************************************************************/
+/* This NPDS modules is free software. You can redistribute it          */
+/* and/or modify it under the terms of the GNU General Public License   */
+/* as published by the Free Software Foundation; either version 2 of    */
+/* the License.                                                         */
+/************************************************************************/
 global $ModInstall;
-#autodoc $name_module: Nom du module #required (no space and exotism)
-$name_module = 'npds_galerie';
+
+#autodoc $name_module: Nom du module
+$name_module = "npds_quiz";
 
 #autodoc $path_adm_module: chemin depuis $ModInstall #required SI admin avec interface
-$path_adm_module = 'admin/adm';
-
-#autodoc $affich: pour l'affichage du nom du module dans l'admin
-$affich='Galerie photo';
-
-#autodoc $icon: icon pour l'admin : c'est un nom de fichier(sans extension) !! #required SI admin avec interface
-$icon='npds_galerie';
+$path_adm_module ='admin/quiz';
+$affich='quiz'; // pour l'affichage du nom du module
+$icon='quiz'; // c'est un nom de fichier(sans extension) !!
 
 #autodoc $sql = array(""): Si votre module doit exécuter une ou plusieurs requêtes SQL, tapez vos requêtes ici.
-#autodoc Attention! UNE requête par élément de tableau! SVP respecter la syntaxe suivante PAS d'espace avant l'instruction
+#autodoc Attention! UNE requête par élément de tableau!
 #autodoc Synopsis: $sql = array("requête_sql_1","requête_sql_2");
 global $NPDS_Prefix;
-$sql = array("CREATE TABLE ".$NPDS_Prefix."tdgal_cat (
-  id int(11) NOT NULL auto_increment,
-  cid int(11) NOT NULL default '0',
-  nom varchar(600) COLLATE utf8mb4_unicode_ci NOT NULL default '',
-  acces tinyint(4) NOT NULL default '0',
-  PRIMARY KEY (id)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;",
-"CREATE TABLE ".$NPDS_Prefix."tdgal_com (
-  id int(11) NOT NULL auto_increment,
-  pic_id int(11) NOT NULL default '0',
-  user varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL default '',
+
+$sql = array("CREATE TABLE ".$NPDS_Prefix."quiz (
+  id int(8) NOT NULL auto_increment,
+  question text COLLATE utf8mb4_unicode_ci NOT NULL,
+  reponse text COLLATE utf8mb4_unicode_ci NOT NULL,
+  propo1 text COLLATE utf8mb4_unicode_ci NOT NULL,
+  propo2 text COLLATE utf8mb4_unicode_ci NOT NULL,
+  propo3 text COLLATE utf8mb4_unicode_ci NOT NULL,
+  propo4 text COLLATE utf8mb4_unicode_ci NOT NULL,
+  propo5 text COLLATE utf8mb4_unicode_ci NOT NULL,
+  propo6 text COLLATE utf8mb4_unicode_ci NOT NULL,
+  categorie varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL default '',
   comment text COLLATE utf8mb4_unicode_ci NOT NULL,
-  comhostname varchar(320) COLLATE utf8mb4_unicode_ci NOT NULL default '',
-  comtimestamp varchar(14) COLLATE utf8mb4_unicode_ci default NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  UNIQUE KEY id_2 (id),
+  KEY id (id)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;",
-"CREATE TABLE ".$NPDS_Prefix."tdgal_gal (
-  id int(11) NOT NULL auto_increment,
-  cid int(11) NOT NULL default '0',
-  nom varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL default '',
-  date varchar(14) COLLATE utf8mb4_unicode_ci default NULL,
-  acces tinyint(4) NOT NULL default '0',
-  PRIMARY KEY (id)
+"INSERT INTO ".$NPDS_Prefix."quiz (id, question, reponse, propo1, propo2, propo3, propo4, propo5, propo6, categorie, comment) VALUES (1, 'Quelle était la couleur du cheval blanc d\'Henri IV ?', 'Je crois qu\'il était blanc voir peut-être un peu moucheté. Il courait souvent dans la gadoue.', 'Je crois qu\'il était bleu. On l\'appelait même le Schtroumpfval d\'Henri IV', 'Je crois qu\'il était blanc voir peut-être un peu moucheté. Il courait souvent dans la gadoue.', 'Je crois qu\'il était vert, et s\'appelait Pédro, et était en fait un âne (Pédro l\'âne vert)', '', '', '', '1', 'Bien sûr qu\'il était blanc. On peut même commenter les réponses, c\'est top ce quiz !'),
+(2, 'Combien y-a-t\'il de pages vues sur le portail NPDS depuis 2001 ?', 'Un truc aussi balaise ? Au moins 5 millions !', 'Oh, il doit bien y en avoir 100, et encore, l\'admin a oublié d\'enlever ses clics !', 'En deux ans, ils ont bien eu 3000 visites quand-même !', 'Un truc aussi balaise ? Au moins 5 millions !', '', '', '', '1', 'Et oui, même 5400000 pages vues au 2 janvier 2004 !'),
+(3, 'On ne doit pas vendre la peau de l\'ours...', '...avant d\'avoir tué la bête', '...non, on ne doit pas la vendre !', '...avant d\'avoir tué la bête', '', '', '', '', '1', 'En même temps, c\'est pas très sympa de vendre la peau des ours.');",
+"CREATE TABLE ".$NPDS_Prefix."quiz_categorie (
+  id int(3) NOT NULL auto_increment,
+  categorie varchar(255)COLLATE utf8mb4_unicode_ci NOT NULL default '',
+  admin varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL default '',
+  tranche int(5) NOT NULL default '0',
+  comment1 text COLLATE utf8mb4_unicode_ci NOT NULL,
+  comment2 text COLLATE utf8mb4_unicode_ci NOT NULL,
+  type int(1) NOT NULL default '0',
+  retenir text COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY  (id),
+  UNIQUE KEY id (id)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;",
-"INSERT INTO ".$NPDS_Prefix."tdgal_gal VALUES (1, -1, 'Import', NULL, -127);",
-"CREATE TABLE ".$NPDS_Prefix."tdgal_img (
-  id int(11) NOT NULL auto_increment,
-  gal_id int(11) NOT NULL default '0',
-  name varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL default '',
-  comment varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL default '',
-  view int(11) NOT NULL default '0',
-  ordre int(11) NOT NULL default '0',
-  noaff int(1) unsigned default '0',
-  PRIMARY KEY (id)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;",
-"CREATE TABLE ".$NPDS_Prefix."tdgal_vot (
-  id int(11) NOT NULL auto_increment,
-  pic_id int(11) NOT NULL default '0',
-  user varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL default '',
-  rating tinyint(4) NOT NULL,
-  ratinghostname varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL default '',
-  ratingtimestamp varchar(14) COLLATE utf8mb4_unicode_ci default NULL,
-  PRIMARY KEY (id)
+"INSERT INTO ".$NPDS_Prefix."quiz_categorie (id, categorie, admin, tranche, comment1, comment2, type, retenir) VALUES (1, 'Quiz de test pour NPDS', 'Arnaud_Latourrette', 2, 'Vous n\'avez pas assez bien répondu alors je vous affiche le commentaire qui vous suggère de mieux travailler !!!', 'Vous avez répondu à au moins 2 questions, c\'est plutôt pas mal, bravo !', 1, 'Là on peut expliquer un peu tout ce qu\'on veut à l\'utilisateur');",
+"CREATE TABLE ".$NPDS_Prefix."quiz_visiteur (
+  numero int(11) NOT NULL auto_increment,
+  nomvisiteur varchar(50) default NULL,
+  mailvisiteur varchar(80) default NULL,
+  reponsesjustes int(4) default NULL,
+  nbquestion int(4) default NULL,
+  date date NOT NULL default '1000-01-01',
+  heure time NOT NULL default '00:00:00',
+  dateheure datetime NOT NULL default '1000-01-01 00:00:00',
+  categorie varchar(20) NOT NULL default '',
+  PRIMARY KEY  (numero),
+  UNIQUE KEY numero (numero)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;");
 
 #autodoc $blocs = array(array(""), array(""), array(""), array(""), array(""), array(""), array(""), array(""), array(""))
 #autodoc                titre      contenu    membre     groupe     index      rétention  actif      aide       description
 #autodoc Configuration des blocs
-$blocs = array(array("Galeries Photo"), array("include#modules/".$name_module."/imgalea.php"), array("0"), array(""), array("1"), array("0"), array("1"), array(""), array("Galeries Photo"));
+
+$blocs = array(array("quiz"), array("include#modules/npds_quiz/quizblock.php"), array("1"), array(""), array("0"), array("0"), array("1"), array(""), array("Quiz"));
+
 
 #autodoc $txtdeb : Vous pouvez mettre ici un texte de votre choix avec du html qui s'affichera au début de l'install
 #autodoc Si rien n'est mis, le texte par défaut sera automatiquement affiché
-$txtdeb = '<br /><strong>Attention:</strong> si le module '.$ModInstall.' est déjà installé sur votre site, veuillez sauter l\'étape de création de la base de données<br /><br />Sinon les tables tdgal_* de votre base de données seront écrasées</b><br /><br />';
+
+$txtdeb = '';
+
 
 #autodoc $txtfin : Vous pouvez mettre ici un texte de votre choix avec du html qui s'affichera à la fin de l'install
-$txtfin = 'Nous vous recommandons de lire le tutorial situé dans le répertoire install du module.<br />Pour les questions et le support rendez vous sur <a href="http://modules.npds.org" target="_blank">modules.npds.org</a>';
 
-#autodoc $end_link: Lien sur lequel sera redirigé l'utilisateur à la fin de l'install (si laissé vide, redirigé sur index.php)
+$txtfin = '';
+
+
+#autodoc $link: Lien sur lequel sera redirigé l'utilisateur à la fin de l'install (si laissé vide, redirigé sur index.php)
 #autodoc N'oubliez pas les '\' si vous utilisez des guillemets !!!
-$end_link = 'admin.php?op=Extend-Admin-SubModule&ModPath='.$ModInstall.'&ModStart=admin/adm';
+
+$end_link = '';
 ?>
